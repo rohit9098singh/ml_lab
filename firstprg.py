3aed9faa7afa79642417cfa9d27f2006f6146af0
rohit@ROHIT <rohitsingh95724@gmail.com>
2025-07-15 22:26:43 +0530

    new push

--- firstprg.py
+++ firstprg.py
@@ -6,18 +6,21 @@ import numpy as np
 print('version of numpy',np.__version__)
 print('version of pandas',pd.__version__)
 
-#numpy operations
 a=[1,2,3,4]
 print(a)
 print('type of a',type(a))
+
+#numpy operations
 b=np.array([1,2,3,4])
 print(b)
 print('type of b',type(b))
 print('dimension of b is',b.ndim)
+
 c=np.array([[1,2,3,4],[5,6,7,8]])
 print(c)
 print('type of c is',type(c))
 print('dimension of c is',c.ndim)
+
 d=np.array([[[1,2,3,4],[5,6,7,8]],[[11,22,33,44],[55,66,77,88]]])
 print(d)
 print('type of c is',type(d))
@@ -28,3 +31,4 @@ data={'name':['alice','bob','charlie'],'sec':['A','B','C']}
 df=pd.DataFrame(data)
 print(df)
 print('columns in dataframe',df.columns)
+
